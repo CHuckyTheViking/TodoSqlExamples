c016f7eaca611a7c72f9adba86dffe42578affdc
Jesper Müllern <mullern23@hotmail.com>
2020-10-12 11:38:38 +0200

    v1

---
+++ TodoSqlDatabaseFirst/Models/Todo.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Diagnostics;
+
+namespace TodoSqlDatabaseFirst.Models
+{
+    public class Todo
+    {
+        public Todo(string activity)
+        {
+            Activity = activity;
+            Completed = false;
+            Created = DateTime.Now;
+        }
+
+        [Key]
+        public int Id { get; set; }
+        [Required]
+        [StringLength(100)]
+        public string Activity { get; set; }
+        public bool Completed { get; set; }
+        public DateTime Created { get; set; }
+    }
+
+
---
+++ TodoSqlDatabaseFirst/Models/TodoContext.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata;
+
+namespace TodoSqlDatabaseFirst.Models
+{
+    public partial class TodoContext : DbContext
+    {
+        public TodoContext()
+        {
+        }
+
+        public TodoContext(DbContextOptions<TodoContext> options)
+            : base(options)
+        {
+        }
+
+        public virtual DbSet<Todo> Todos { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            if (!optionsBuilder.IsConfigured)
+            {
+                optionsBuilder.UseSqlServer("Data Source=(LocalDB)\\MSSQLLocalDB;AttachDbFilename=C:\\Users\\Jesper\\Documents\\TodoSqlDatabaseFirst.mdf;Integrated Security=True;Connect Timeout=30");
+            }
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            OnModelCreatingPartial(modelBuilder);
+        }
+
+        partial void OnModelCreatingPartial(ModelBuilder modelBuilder);
+    }
+}
